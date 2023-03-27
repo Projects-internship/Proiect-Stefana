97386485a858a29b65f528d33c88ac4ae1cbba60
StefanaNistor <nistorstefana21@stud.ase.ro>
2023-03-27 14:54:56 +0300

    ajutor

--- frontend/modules-js/profile-script.js
+++ frontend/modules-js/profile-script.js
@@ -1,9 +1,10 @@
 async function logout(url = "http://localhost:5000/logout", data = {}) {
     const response = await fetch(url, {
         method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
+        headers : { 
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+           },
         body: JSON.stringify(data),
     });
     return response.json();
